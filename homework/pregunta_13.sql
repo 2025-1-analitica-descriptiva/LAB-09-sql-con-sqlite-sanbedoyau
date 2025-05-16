1e39f71ff08e8a621584ff84bf8b42b1d78f4ab1
sanbedoyau <sanbedoya@unal.edu.co>
2025-05-16 17:38:47 -0500

    Lab Done x2

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,6 +43,6 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, ROUND(AVG(c12), 2) as "avg(c12)" FROM tbl1 -- Así para forzar a que pase el test :)
+SELECT K0, avg(c12) FROM tbl1
 WHERE c13 > 400
 GROUP BY K0;
